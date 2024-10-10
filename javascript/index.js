314bcebd13a8ee9fc4e5f008a773f6a054260fee
TheBrain08 <t-bilharz@online.de>
2024-10-10 20:09:39 +0200

    add lightmode for indexpage

--- javascript/index.js
+++ javascript/index.js
@@ -187,3 +187,26 @@ function sendMessage() {
     }
     alert("Danke für deine Nachricht")
 }
+
+
+let lightmode = localStorage.getItem('lightmode');
+const themeSwitch = document.getElementById('themeSwitch');
+
+function enableLightmode() {
+    document.body.classList.add('lightmode');
+    localStorage.setItem('lightmode', 'active');
+    document.getElementById('logo').src = "../img/logo-lightmode.png";
+}
+
+function disableLightmode() {
+    document.body.classList.remove('lightmode');
+    localStorage.setItem('lightmode', null);
+    document.getElementById('logo').src = "../img/logo.png";
+}
+
+if (lightmode === "active") enableLightmode()
+
+themeSwitch.addEventListener("click", () => {
+    lightmode = localStorage.getItem('lightmode')
+    lightmode !== "active" ? enableLightmode() : disableLightmode()
+});
