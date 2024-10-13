28e047a524d8a7341a8d1bc9669f05e9a12be96f
TheBrain08 <t-bilharz@online.de>
2024-10-13 15:59:06 +0200

    maintenance site lightmode

--- javascript/index.js
+++ javascript/index.js
@@ -171,34 +171,3 @@ anchorTags.forEach(anchor => {
     anchor.addEventListener('mouseover', hoverHandler);
     anchor.addEventListener('mouseout', resetHandler);
 });
-
-
-
-
-let lightmode = localStorage.getItem('lightmode');
-const themeSwitch = document.getElementById('themeSwitch');
-const themeSwitchSM = document.getElementById('themeSwitchSM');
-
-function enableLightmode() {
-    document.body.classList.add('lightmode');
-    localStorage.setItem('lightmode', 'active');
-    document.getElementById('logo').src = "../img/logo-lightmode.png";
-}
-
-function disableLightmode() {
-    document.body.classList.remove('lightmode');
-    localStorage.setItem('lightmode', null);
-    document.getElementById('logo').src = "../img/logo.png";
-}
-
-if (lightmode === "active") enableLightmode()
-
-themeSwitch.addEventListener("click", () => {
-    lightmode = localStorage.getItem('lightmode')
-    lightmode !== "active" ? enableLightmode() : disableLightmode()
-});
-
-themeSwitchSM.addEventListener("click", () => {
-    lightmode = localStorage.getItem('lightmode');
-    lightmode !== "active" ? enableLightmode() : disableLightmode();
-});
